1f2884313e77e1ad5e6fea58e1be5b806568eb07
Thibault Lestang <thibault.lestang@protonmail.com>
2022-05-24 15:13:41 +0100

    Rename model function

--- fit_data.py
+++ fit_data.py
@@ -6,7 +6,7 @@ import sys
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
-from model import model
+from model import model_fun
 
 filename = sys.argv[1]
 try:
@@ -16,7 +16,7 @@ except OSError:
 
 plt.plot(data[:, 0], data[:, 1], "ro")
 
-popt, pcov = curve_fit(model, data[:, 0], data[:, 1], p0=(1, 0.2))
+popt, pcov = curve_fit(model_fun, data[:, 0], data[:, 1], p0=(1, 0.2))
 xmin = np.min(data[:, 0])
 xmax = np.max(data[:, 0])
 x = np.linspace(xmin, xmax, 50)
--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 import numpy as np
 
 
-def model(x, p1, p2):
+def model_fun(x, p1, p2):
     return p1 * np.cos(p2 * x) + p2 * np.sin(p1 * x)

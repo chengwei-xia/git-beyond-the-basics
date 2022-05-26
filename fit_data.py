be85a8cb70f241613e07e70523e225a91c7f451a
Thibault Lestang <thibault.lestang@protonmail.com>
2022-05-26 15:13:52 +0100

    Only plot if --plot option is specified

--- fit_data.py
+++ fit_data.py
@@ -14,12 +14,12 @@ try:
 except OSError:
     raise FileNotFoundError(f"File {filename} not found")
 
-plt.plot(data[:, 0], data[:, 1], "r*", label="data points")
-
 popt, pcov = curve_fit(model, data[:, 0], data[:, 1], p0=(1, 0.2))
-xmin = np.min(data[:, 0])
-xmax = np.max(data[:, 0])
-x = np.linspace(xmin, xmax, 50)
-plt.plot(x, model(x, *popt), "b", linewidth=2, label="fitted model")
-plt.legend()
-plt.show()
+
+if "--plot" in sys.argv:
+    plt.plot(data[:, 0], data[:, 1], "r*", label="data points")
+    xmin = np.min(data[:, 0])
+    xmax = np.max(data[:, 0])
+    x = np.linspace(xmin, xmax, 50)
+    plt.plot(x, model(x, *popt), "b", linewidth=2, label="fitted model")
+    plt.show()

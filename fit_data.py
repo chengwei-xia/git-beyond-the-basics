4b3cc15dd80ea67267f560018eb04fec6742397a
Thibault Lestang <thibault.lestang@protonmail.com>
2022-05-26 15:51:57 +0100

    Check for command line arguments and remove temp variables xmin and xmax

--- fit_data.py
+++ fit_data.py
@@ -8,6 +8,9 @@ import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 from model import model
 
+if len(sys.argv) < 2:
+    raise RuntimeError("Missing filename argument")
+
 filename = sys.argv[1]
 try:
     data = np.loadtxt(filename, delimiter=",", skiprows=1)
@@ -17,9 +20,7 @@ except OSError:
 popt, pcov = curve_fit(model, data[:, 0], data[:, 1], p0=(1, 0.2))
 
 if "--plot" in sys.argv:
-    plt.plot(data[:, 0], data[:, 1], "r*", label="data points")
-    xmin = np.min(data[:, 0])
-    xmax = np.max(data[:, 0])
-    x = np.linspace(xmin, xmax, 50)
+    plt.plot(data[:, 0], data[:, 1], color="r", "*", label="data points")
+    x = np.linspace(np.min(data[:, 0]), np.max(data[:, 0]), 50)
     plt.plot(x, model(x, *popt), "b", linewidth=2, label="fitted model")
     plt.show()

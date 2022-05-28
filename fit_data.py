d0055ed842a7482e416bb9f6e4c56b66534a86b4
Thibault Lestang <thibault.lestang@protonmail.com>
2022-05-28 17:48:26 +0100

    Warn if --save is set but not --plot

--- fit_data.py
+++ fit_data.py
@@ -3,6 +3,7 @@ Read data points and plot them along fitted model
 Usage: python fit_data.py data_file
 """
 import sys
+from warnings import warn
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
@@ -16,6 +17,16 @@ if "--help" in sys.argv:
     print("usage: python {sys.argv[0]} file [--plot]")
     sys.exit()
 
+
+draw_plot = False
+save_plot = False
+if "--plot" in sys.argv:
+    draw_plot = True
+if "--save" in sys.argv:
+    save_plot = True
+if save_plot and not draw_plot:
+    warn("Ignoring --save option. Use --plot to enable plotting")
+
 filename = sys.argv[1]
 try:
     data = np.loadtxt(filename, delimiter=",", skiprows=1)
@@ -25,10 +36,10 @@ except OSError:
 popt, pcov = curve_fit(model, data[:, 0], data[:, 1], p0=(1, 0.2))
 logging.info("p1 = {popt[0]}, p2 = {popt[1]}")
 
-if "--plot" in sys.argv:
+if draw_plot in sys.argv:
     plt.plot(data[:, 0], data[:, 1], color="r", "*", label="data points")
     x = np.linspace(np.min(data[:, 0]), np.max(data[:, 0]), 50)
     plt.plot(x, model(x, *popt), "b", linewidth=2, label="fitted model")
-    if "--save" in sys.argv:
+    if save_plot in sys.argv:
         plt.savefig("figure.png")
     plt.show()

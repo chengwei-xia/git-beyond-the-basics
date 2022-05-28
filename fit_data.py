092e32bfd3276dedd483e7b48342c38a462df42b
Thibault Lestang <thibault.lestang@protonmail.com>
2022-05-28 18:05:20 +0100

    use argparse library to parse command line arguments

--- fit_data.py
+++ fit_data.py
@@ -3,12 +3,19 @@ Read data points and plot them along fitted model
 Usage: python fit_data.py data_file
 """
 import sys
+import argparse
 from warnings import warn
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 from model import model
 import logging
+
+parser = argparse.ArgumentParser(description="Plot data and fitted model")
+parser.add_argument("filename", type=str, help="Name of data file")
+parser.add_argument("--plot", action="store_true")
+parser.add_argument("--save", action="store_true")
+args = parser.parse_args()
 
 if len(sys.argv) < 2:
     raise RuntimeError("Missing filename argument")
@@ -17,25 +24,21 @@ if "--help" in sys.argv:
     print("usage: python {sys.argv[0]} file [--plot]")
     sys.exit()
 
-
-draw_plot = True if "--plot" in sys.argv else False
-save_plot = True if "--save" in sys.argv else False
-if save_plot and not draw_plot:
+if args.save and not args.plot:
     warn("Ignoring --save option. Use --plot to enable plotting")
 
-filename = sys.argv[1]
 try:
-    data = np.loadtxt(filename, delimiter=",", skiprows=1)
+    data = np.loadtxt(args.filename, delimiter=",", skiprows=1)
 except OSError:
-    raise FileNotFoundError(f"File {filename} not found")
+    raise FileNotFoundError(f"File {args.filename} not found")
 
 popt, pcov = curve_fit(model, data[:, 0], data[:, 1], p0=(1, 0.2))
 logging.info("p1 = {popt[0]}, p2 = {popt[1]}")
 
-if draw_plot in sys.argv:
+if args.plot in sys.argv:
     plt.plot(data[:, 0], data[:, 1], color="r", "*", label="data points")
     x = np.linspace(np.min(data[:, 0]), np.max(data[:, 0]), 50)
     plt.plot(x, model(x, *popt), "b", linewidth=2, label="fitted model")
-    if save_plot in sys.argv:
+    if args.save:
         plt.savefig("figure.png")
     plt.show()

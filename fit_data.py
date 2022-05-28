410c40a4d161dab71e3908bc260591a87edfc890
Thibault Lestang <thibault.lestang@protonmail.com>
2022-05-28 17:36:36 +0100

    Add --save option to save plot as png image

--- fit_data.py
+++ fit_data.py
@@ -30,3 +30,5 @@ if "--plot" in sys.argv:
     x = np.linspace(np.min(data[:, 0]), np.max(data[:, 0]), 50)
     plt.plot(x, model(x, *popt), "b", linewidth=2, label="fitted model")
     plt.show()
+    if "--save" in sys.argv:
+        plt.savefig("figure.png")
